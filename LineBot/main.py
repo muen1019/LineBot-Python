2637a4f3826b8b273c719ed3d78c731419c8d5aa
muen1019 <91111746+muen1019@users.noreply.github.com>
2024-05-28 20:00:32 +0800

    加入記帳相關功能
    好久以前加入的功能，但一直沒有在這裡更新

--- LineBot/main.py
+++ LineBot/main.py
@@ -13,6 +13,12 @@ from googleapiclient.http import MediaFileUpload
 from httplib2 import Http
 from oauth2client import file, client, tools
 from random import randint
+from flask_restful import Resource, Api
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials as sac
+import datetime as dt
+import pytz
+# from revChatGPT.V1 import Chatbot 
 
 Debug_Mode = False
 app = Flask(__name__)
@@ -26,6 +32,16 @@ ok_sticker = [["6362", "11087920"], ["8525", "16581290"], ["11539", "52114113"]]
 # 雲端文件存放位置
 folder_id = "1xgbysfK5zTz1CLDeKZaMiuG1HiNS8G4S"
 folder_url = "https://drive.google.com/folderview?id=1xgbysfK5zTz1CLDeKZaMiuG1HiNS8G4S"
+# 記帳相關資料
+auth_json = "autoupload-336306-711b168145e6.json"
+gs_scopes = ["https://spreadsheets.google.com/feeds"]
+spreadsheet_key = "1MfgcbA0lW58HaFYFsogG86jXCVDj2RLzT0_LSZcKm_w"
+muan_spreadsheet_key = "1DD5pvHvoYi5hYhSEVkdjzddX-GaobnrJxO6Hpl8wxv4"
+my_user_id = "U3e5359d656fc6d1d6610ddcb33323bde"
+muan_user_id = "U56745f8381f264a269dbca24eb4c6977"
+mom_user_id = "U587c68afa4b2167d47d76d72dcd7a0d3"
+
+
 # line bot 初始化
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -67,7 +83,7 @@ def upload_file(fileName, path):
         flow = client.flow_from_clientsecrets("credentials.json", SCOPES)
         creds = tools.run_flow(flow, store)
     
-    service = build("drive", "v3", http = creds.authorize(Http()))
+    service = build("drive", "v3", http = creds.authorize(Http()),  static_discovery=False)
     print("開始上傳檔案")
     start = time.time()
     file_metada = {"name": fileName, "parents": [folder_id]}
@@ -103,6 +119,120 @@ def Can_Send(event):
     if event.source.type == "group" and event.source.group_id in From(): return True
     return False
 
+# 在次數達上限的時候 使用line notify傳送通知
+def LineNotify(token, msg):
+    headers = {
+        "Authorization": "Bearer " + token, 
+        "Content-Type" : "application/x-www-form-urlencoded"
+    }
+
+    payload = {'message': msg}
+    notify = requests.post("https://notify-api.line.me/api/notify", headers = headers, params = payload)
+    return notify.status_code
+
+# 記帳
+def track_expense(l, user_id):
+    # 認證
+    cr = sac.from_json_keyfile_name(auth_json)
+    gs_client = gspread.authorize(cr)
+    # 抓取現在時間
+    now = dt.datetime.now(pytz.timezone("Asia/Taipei"))
+    # 是否為約旦記帳模式
+    # if now.month == 8 and now.day >= 5 and now.day <= 15 and user_id == my_user_id:
+    #     jordan_mode = 1
+    #     now = dt.datetime.now(pytz.timezone("Asia/Amman"))
+    # else:
+    #     jordan_mode = 0
+    jordan_mode = 0
+    # 另外補齊分鐘的0
+    if now.minute < 10:
+        minute = "0" + str(now.minute)
+    else:
+        minute = now.minute
+    # 開啟試算表
+    if user_id == my_user_id:
+        sheet = gs_client.open_by_key(spreadsheet_key)
+        person = "沐恩"
+    else:
+        sheet = gs_client.open_by_key(muan_spreadsheet_key)
+        person = "沐安"
+    # 判別是否為爸媽的錢
+    if len(l) > 2 and l[2] == "爸媽":
+        is_parent = 1
+        if jordan_mode:
+            wks_name = "2023/8約旦行_爸媽"
+        else:
+            wks_name = "爸媽的錢"
+    else:
+        is_parent = 0
+        if jordan_mode:
+            wks_name = "2023/8約旦行"
+        else:
+            wks_name = f"{str(now.year)}/{str(now.month)}"
+    try:
+        wks = sheet.worksheet(wks_name)
+    except:
+        # 建立新workbook
+        wks = sheet.add_worksheet(wks_name, 0, 0)
+        row_value = ["日期", "時間", "類別", "項目", "收入", "支出", "總計"]
+        wks.append_row(row_value)
+    # 判別收入或支出
+    if l[0].replace(".", "").isdigit():
+        if is_parent: row_value = [f"{now.month}/{now.day}", f"{now.hour}:{minute}", l[1], l[0], ""]
+        else: row_value = [f"{now.month}/{now.day}", f"{now.hour}:{minute}", l[2], l[1], l[0], ""]
+    else:
+        if is_parent: row_value = [f"{now.month}/{now.day}", f"{now.hour}:{minute}", l[0], "", l[1]]
+        else: row_value = [f"{now.month}/{now.day}", f"{now.hour}:{minute}", l[2], l[0], "", l[1]]
+    # 加入新資料
+    all_data = wks.get_all_values()
+    # 行數
+    length = len(all_data)
+    # 計算總和
+    if length == 1:
+        if is_parent: row_value.append(f"=SUM(D{length + 1}, -E{length + 1})")
+        else: row_value.append(f"=SUM(E{length + 1}, -F{length + 1})")
+    else:
+        if is_parent: row_value.append(f"=SUM(D{length + 1}, -E{length + 1}, F{length})")
+        else: row_value.append(f"=SUM(E{length + 1}, -F{length + 1}, G{length})")
+    wks.append_row(row_value, value_input_option="USER_ENTERED")
+    # 回傳剩餘金額
+    all_data = wks.get_all_values()
+    lst = all_data[-1][-1]
+    # 回傳資料給媽媽
+    if is_parent:
+        # 判別收入或支出
+        if l[0].isdigit():
+            line_bot_api.push_message(mom_user_id, TextSendMessage(f"{person}增加了{int(l[0])}，目前剩餘{lst}元"))
+        else:
+            line_bot_api.push_message(mom_user_id, TextSendMessage(f"{person}購買了{l[0]}，花了{l[1]}元，剩餘{lst}元"))
+    return lst
+    
+
+# def gpt_response(gpt_prompt):
+#     openai.api_key = os.environ["OPENAI_API_KEY"]
+#     response = openai.Completion.create(
+#         # engine = "text-curie-001",
+#         engine = "text-davinci-002",
+#         prompt = gpt_prompt,
+#         temperature=0.9,
+#         max_tokens=2000,
+#         top_p=0.5,
+#         frequency_penalty=0.0,
+#         presence_penalty=0.0,
+#         # stop=["\n"]
+#     )
+#     return response["choices"][0]["text"][2:]
+
+# def gpt_response_web(gpt_prompt):
+#     chatbot = Chatbot(config={
+#         "access_token": os.environ["ChatGPT_Access_Token"]
+#     })
+#     response = ""
+#     for data in chatbot.ask(gpt_prompt):
+#         response = data["message"]
+#     return response
+
+
 
 @handler.add(MessageEvent)
 def handle_message(event):
@@ -120,10 +250,25 @@ def handle_message(event):
             else:
                 profile = line_bot_api.get_profile(event.source.user_id)
             line_bot_api.reply_message(event.reply_token, TextSendMessage(str(profile.display_name) + "好電⚡"))
+            
         elif event.source.type == "user":
-            line_bot_api.reply_message(
-                event.reply_token,
-                TextSendMessage(text=event.message.text))
+            # 記帳
+            if event.source.user_id == my_user_id or event.source.user_id == muan_user_id:
+                l = list(event.message.text.split())
+                if l[0].replace(".", "").isdigit() or (len(l) > 1 and l[1].replace(".", "").isdigit()):
+                    lst = track_expense(l, event.source.user_id)
+                    line_bot_api.reply_message(event.reply_token, TextSendMessage(text = f"記帳完成!剩餘{lst}元"))
+                else:
+                   # line_bot_api.reply_message(
+                   #  event.reply_token,                TextSendMessage(text=gpt_response_web(event.message.text))) 
+                    line_bot_api.reply_message(
+                event.reply_token,                TextSendMessage(text=event.message.text))
+            else:
+                # line_bot_api.reply_message(
+                # event.reply_token,                TextSendMessage(text=gpt_response_web(event.message.text))) 
+                line_bot_api.reply_message(
+                event.reply_token,                 TextSendMessage(text=event.message.text))
+            
     if event.message.type == "image":
         content = line_bot_api.get_message_content(event.message.id)
         with open(os.path.join("storage", "temp.jpg"), "wb") as f:
@@ -134,21 +279,25 @@ def handle_message(event):
         if Debug_Mode or Can_Send(event):
             content = line_bot_api.get_message_content(str(event.message.id))
             fileName = event.message.file_name
-            with open(os.path.join("storage", fileName),
-                      "wb") as f:
+            with open(os.path.join("storage", fileName), "wb") as f:
                 for chunk in content.iter_content():
                     f.write(chunk)
             value = upload_file(fileName, os.path.join("storage", fileName))
             if value["success"] == False:
+               try: 
                 line_bot_api.push_message("U3e5359d656fc6d1d6610ddcb33323bde", TextSendMessage("Token已過期 請盡速更新並重新傳檔案"))
+               except:
+                    LineNotify(os.environ["LINE_NOTIFY_TOKEN"], "Token已過期 請盡速更新並重新傳檔案")
             else:
                 to = To()
                 fileURL = value["fileURL"]
-                for i in to:
-                    line_bot_api.push_message(i, TextSendMessage("大家好，我是(已經退休的)物理小老師的機器人\n以下是老師要傳給同學的檔案：\n" + fileURL + "\n其他檔案：\n" + folder_url))
-                sticker = ok_sticker[randint(0, 2)]
-                line_bot_api.reply_message(event.reply_token, StickerSendMessage(sticker[0], sticker[1]))
-                sticker = ok_sticker[randint(0, 2)]
+                try:                
+                    for i in to:
+                        line_bot_api.push_message(i, TextSendMessage("大家好，我是物理小老師的機器人\n以下是老師要傳給同學的檔案：\n" + fileURL + "\n其他檔案：\n" + folder_url))
+                    sticker = ok_sticker[randint(0, 2)]
+                except:
+                    pass
+                    # LineNotify(os.environ["LINE_NOTIFY_TOKEN"], f"無法正常傳送訊息!\n以下是欲傳送的文字:\n\n大家好，我是物理小老師\n以下是老師要傳給同學的檔案：\n{fileURL}\n其他檔案：\n{folder_url}")
                 line_bot_api.reply_message(event.reply_token, StickerSendMessage(sticker[0], sticker[1]))
 
 @handler.add(JoinEvent)
@@ -169,6 +318,8 @@ def handler_follow(event):
 def running():
     return "running"
 
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    try:
+        app.run(host="0.0.0.0")
+    except:
+        os.system("kill 1")
