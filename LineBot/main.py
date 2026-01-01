1c0e9ed8fe86b8c9d87854488eb8e5251a1682bf
CHIU, MU-EN <91111746+muen1019@users.noreply.github.com>
2026-01-02 07:03:24 +0800

    testing for supporting English Bible title

--- LineBot/main.py
+++ LineBot/main.py
@@ -507,7 +507,7 @@ def send_bible():
             print(video_url)
             return f"{current_date.strftime('%Y/%m/%d')} {keyword}\n{video_url}"
     # return f"{current_date.strftime('%Y/%m/%d')} {keyword}\nhttps://youtu.be/watch?v={result[0]['id']}"
-    return f"{current_date.strftime('%Y/%m/%d')} {keyword}\n找不到符合的影片連結"
+    return f"{current_date.strftime('%Y/%m/%d')} {keyword}\n找不到符合的影片連結\n{result[0]['title']}"
 
 # 讀取當天跑步資訊
 def get_today_run_info():
